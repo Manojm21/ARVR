d167cc5a3b3bc910c4f2364636b3c2d17d9506ea
Manoj M <109453273+Manojm21@users.noreply.github.com>
2025-03-16 22:43:22 +0530

    Update program1.cs

--- Program1/program1.cs
+++ Program1/program1.cs
@@ -1,66 +1,46 @@
 using UnityEngine;
 
-public class AdvancedTransform : MonoBehaviour
+public class ObjectMover : MonoBehaviour
 {
-    public GameObject myCube;
-    public GameObject mySphere;
-    public GameObject myPlane;
 
-    public float cubeMoveSpeed = 2f;
-    public float sphereMoveSpeed = 1.5f;
-    public float planeMoveSpeed = 3f;
-
-    public float rotationSpeed = 50f;
-    public float scaleSpeed = 0.5f;
+public GameObject cube;
+    public float moveSpeedX = 5f;
+    public float moveSpeedZ = 3f;
+    public float rotationSpeedY = 45f;
+    public float scaleFactorX = 1.1f;
 
     void Update()
     {
-        // Cube transformations
-        if (Input.GetKey(KeyCode.C)) // Move Cube
+        // Move object in different directions
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            myCube.transform.position += new Vector3(cubeMoveSpeed * Time.deltaTime, 0, 0);  // Move in X direction
+            cube.transform.position += Vector3.forward * moveSpeedZ * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            cube.transform.position += Vector3.back * moveSpeedZ * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            cube.transform.position += Vector3.left * moveSpeedX * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            cube.transform.position += Vector3.right * moveSpeedX * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.X)) // Scale Cube
+        // Rotate object along Y-axis
+        if (Input.GetKey(KeyCode.R))
         {
-            myCube.transform.localScale += new Vector3(scaleSpeed * Time.deltaTime, 0, 0);  // Scale on X axis
+            cube.transform.Rotate(0, rotationSpeedY * Time.deltaTime, 0);
         }
 
-        if (Input.GetKey(KeyCode.Z)) // Rotate Cube
+        // Scale object along X-axis
+        if (Input.GetKeyDown(KeyCode.X))
         {
-            myCube.transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);  // Rotate along X axis
-        }
-
-        // Sphere transformations
-        if (Input.GetKey(KeyCode.S)) // Move Sphere
-        {
-            mySphere.transform.position += new Vector3(0, sphereMoveSpeed * Time.deltaTime, 0);  // Move in Y direction
-        }
-
-        if (Input.GetKey(KeyCode.W)) // Scale Sphere
-        {
-            mySphere.transform.localScale += new Vector3(0, scaleSpeed * Time.deltaTime, 0);  // Scale on Y axis
-        }
-
-        if (Input.GetKey(KeyCode.A)) // Rotate Sphere
-        {
-            mySphere.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);  // Rotate along Y axis
-        }
-
-        // Plane transformations
-        if (Input.GetKey(KeyCode.P)) // Move Plane
-        {
-            myPlane.transform.position += new Vector3(0, 0, planeMoveSpeed * Time.deltaTime);  // Move in Z direction
-        }
-
-        if (Input.GetKey(KeyCode.Q)) // Scale Plane
-        {
-            myPlane.transform.localScale += new Vector3(0, 0, scaleSpeed * Time.deltaTime);  // Scale on Z axis
-        }
-
-        if (Input.GetKey(KeyCode.E)) // Rotate Plane
-        {
-            myPlane.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);  // Rotate along Z axis
+            cube.transform.localScale = new Vector3(transform.localScale.x * scaleFactorX,
+                                               transform.localScale.y,
+                                               transform.localScale.z);
         }
     }
 }
